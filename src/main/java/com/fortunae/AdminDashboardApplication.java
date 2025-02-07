1b0b6361e4bfd297d97d96b906e8dcfe5cda8944
Baliqis <sulaimabaliqis@gmail.com>
2025-02-07 15:47:14 +0100

    remove port from application.properties

--- src/main/java/com/fortunae/AdminDashboardApplication.java
+++ src/main/java/com/fortunae/AdminDashboardApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = "com.fortunae.data.repository")
 @SpringBootApplication
 public class AdminDashboardApplication {
     public static void main(String[] args) {
